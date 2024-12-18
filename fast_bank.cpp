c84d1790835363713561810894a8cabb9b201361
Vladislav Borzyaev <vladislavborzyaev@MacBook-Air-Sekki.local>
2024-12-18 17:42:47 +0300

    Drozd EvgehS Yato complite

--- fast_bank.cpp
+++ fast_bank.cpp
@@ -37,10 +37,11 @@ public:
     
     bool getStatus() const { return isBusy; }
 
-    void processRequest(Client* client) {
+    string processRequest(Client* client) {
         isBusy = true;
         cout << "Оператор " << id << " обрабатывает запрос клиента ID " << client->getId() << endl;
         isBusy = false;
+        return "Оператор " + to_string(id) + " обрабатал запрос клиента ID " + to_string(client->getId()) + '\n';
     }
 };
 
@@ -157,202 +158,61 @@ class Deque {
         }
 };
 
+// Реализация стека
+class Stack {
+    string* operations;
+    size_t size;
 
+    public:
+        Stack() {operations = nullptr; size = 0;}
+        ~Stack(){delete[] operations;}
 
-// // Реализация стека
-// class Stack {
-// private:
-//     struct Node {
-//         string operation;
-//         Node* next;
-//         Node(const string& operation) : operation(operation), next(nullptr) {}
-//     };
+        void push(string oper) {
+            size += 1;
+            string* tmp = new string [size];
+            for (size_t i = 0; i < size-1; ++i){
+                tmp[i] = operations[i];
+            }
 
-//     Node* top;
+            tmp[size-1] = oper;
 
-// public:
-//     Stack() : top(nullptr) {}
+            if (operations) delete[] operations;
+            operations = tmp;
+        }
 
-//     ~Stack() {
-//         while (top) {
-//             Node* temp = top;
-//             top = top->next;
-//             delete temp;
-//         }
-//     }
+        void pop() {
+            size -= 1;
+            string* tmp = new string [size];
+            for (size_t i = 0; i < size; ++i){
+                tmp[i] = operations[i+1];
+            }
 
-//     void push(const string& operation) {
-//         Node* newNode = new Node(operation);
-//         newNode->next = top;
-//         top = newNode;
-//     }
+            if (operations) delete[] operations;
+            operations = tmp;
+        }
 
-//     string pop() {
-//         if (!top) return "";
-//         Node* temp = top;
-//         string operation = temp->operation;
-//         top = top->next;
-//         delete temp;
-//         return operation;
-//     }
+        string top() {
+            if (operations) return operations[size-1]; 
+            else throw std::out_of_range("Долбаёб он пустой");
+        } 
+};
 
-//     string peek() const {
-//         return top ? top->operation : "";
-//     }
-
-//     bool isEmpty() const {
-//         return top == nullptr;
-//     }
-
-//     void displayStack() const {
-//         Node* temp = top;
-//         while (temp) {
-//             cout << temp->operation << endl;
-//             temp = temp->next;
-//         }
-//     }
-// };
-
-
-
-// // Класс BankSystem
-// class BankSystem {
-// private:
-//     Queue lowPriorityQueue;
-//     Deque highPriorityDeque;
-//     Stack history;
-//     Operator* operators;
-//     int operatorCount;
-//     int clientIdCounter;
-
-// public:
-//     BankSystem(int operatorCount) : operatorCount(operatorCount), clientIdCounter(1) {
-//         operators = new Operator[operatorCount];
-//         for (int i = 0; i < operatorCount; ++i) {
-//             operators[i] = Operator(i + 1);
-//         }
-//     }
-
-//     ~BankSystem() {
-//         delete[] operators;
-//     }
-
-//     void addClient(const string& requestType) {
-//         Client* client = new Client(clientIdCounter++, requestType);
-//         if (requestType == "high") {
-//             highPriorityDeque.addRear(client);
-//         } else {
-//             lowPriorityQueue.enqueue(client);
-//         }
-//         history.push("Добавлен клиент ID " + to_string(client->getId()));
-//     }
-
-//     void processRequest() {
-//         for (int i = 0; i < operatorCount; ++i) {
-//             if (!operators[i].getStatus()) {
-//                 Client* client = nullptr;
-//                 if (!highPriorityDeque.isEmpty()) {
-//                     client = highPriorityDeque.removeFront();
-//                 } else if (!lowPriorityQueue.isEmpty()) {
-//                     client = lowPriorityQueue.dequeue();
-//                 }
-
-//                 if (client) {
-//                     operators[i].processRequest(client);
-//                     history.push("Обработан запрос клиента ID " + to_string(client->getId()));
-//                     delete client;
-//                     return;
-//                 } else {
-//                     cout << "Нет запросов для обработки." << endl;
-//                     return;
-//                 }
-//             }
-//         }
-//         cout << "Все операторы заняты." << endl;
-//     }
-
-//     void showQueues() const {
-//         cout << "Очередь низкого приоритета:" << endl;
-//         lowPriorityQueue.displayQueue();
-//         cout << "Очередь высокого приоритета:" << endl;
-//         highPriorityDeque.displayDeque();
-//     }
-
-//     void showHistory() const {
-//         cout << "История операций:" << endl;
-//         history.displayStack();
-//     }
-
-//     void undoLastOperation() {
-//         if (!history.isEmpty()) {
-//             string operation = history.pop();
-//             cout << "Отмена операции: " << operation << endl;
-//         } else {
-//             cout << "История операций пуста." << endl;
-//         }
-//     }
-// };
 
 // Главная функция
 int main() {
-    // // BankSystem bank(2);
-    // int choice;
-
-    // do {
-    //     cout << "=== Система обслуживания клиентов банка ===" << endl;
-    //     cout << "1. Добавить клиента в систему" << endl;
-    //     cout << "2. Обработать запрос клиента" << endl;
-    //     cout << "3. Просмотреть состояние очередей" << endl;
-    //     cout << "4. Показать историю завершенных операций" << endl;
-    //     cout << "5. Отменить последнюю операцию" << endl;
-    //     cout << "6. Выход" << endl;
-    //     cout << "Ваш выбор: ";
-    //     cin >> choice;
-
-    //     switch (choice) {
-    //     case 1: {
-    //         string requestType;
-    //         cout << "Введите тип запроса (low/high): ";
-    //         cin >> requestType;
-    //         bank.addClient(requestType);
-    //         break;
-    //     }
-    //     case 2:
-    //         bank.processRequest();
-    //         break;
-    //     case 3:
-    //         bank.showQueues();
-    //         break;
-    //     case 4:
-    //         bank.showHistory();
-    //         break;
-    //     case 5:
-    //         bank.undoLastOperation();
-    //         break;
-    //     case 6:
-    //         cout << "Выход из программы." << endl;
-    //         break;
-    //     default:
-    //         cout << "Неверный выбор. Попробуйте снова." << endl;
-    //     }
-    // } while (choice != 6);
-
-    // return 0;
+    Operator* operators[2];
+    Stack stack_;
+    
     Client client(0, "low");
     Client client_1(1, "high");
-    Deque line_1;
-    line_1.front_push(&client);
-    line_1.front_push(&client_1);
-    line_1.out_line();
-    line_1.back_push(&client);
-    line_1.back_push(&client_1);
-    line_1.out_line();
     
-    line_1.back_pop();
-    line_1.front_pop();
-    line_1.out_line();
-    
+    Operator operator_(0);
+    Operator operator_1(1);
+    operators[0] = &operator_;
 
+    cout << stack_.top();
+    stack_.push(operators[0]->processRequest(&client_1));
+    cout << stack_.top();
 
     return 0;
 }
