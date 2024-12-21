180a04a1e307f8dc2db3ca2c4517ab73635f81e2
Evgen <sidzeka2000@gmail.com>
2024-12-21 21:24:39 +0300

    Вроде работает

--- fast_bank.cpp
+++ fast_bank.cpp
@@ -61,7 +61,9 @@ public:
         isBusy = true;                                      
         cout << "Оператор " << id << " обрабатывает запрос клиента ID " << client->getId() << endl;
         isBusy = false;
-        return "Оператор " + to_string(id) + " обрабатал запрос клиента ID " + to_string(client->getId()) + '\n';
+        std::string std_id = to_string(id), std_client_id = to_string(client->getId());
+        delete client;
+        return "Оператор " + std_id + " обрабатал запрос клиента ID " + std_client_id + '\n';
     }
 };
 
@@ -113,6 +115,7 @@ class Queue {
 class Deque {
     Client** data;                                          // Массив указателей на клиентов
     size_t size;                                            // Размер массива
+    friend class BankSystem;
 
     public:
         Deque() {data = nullptr; size = 0;}                 // конуструктор
@@ -149,27 +152,31 @@ class Deque {
             data = tmp;
         }
                                                             // удаление клиента из начала очереди
-        void front_pop() {
+        Client* front_pop() {
             size -= 1;
             Client** tmp = new Client* [size];
             for (size_t i = 0; i < size; ++i){
                 tmp[i] = data[i+1];
             }
 
+            Client* client = data[0];
             if (data) delete[] data;
             data = tmp;
+            return client;
         }
 
                                                             // удаление клиента из конца очереди
-        void back_pop() {
+        Client* back_pop() {
             size -= 1;
             Client** tmp = new Client* [size];
             for (size_t i = 0; i < size; ++i){
                 tmp[i] = data[i];
             }
 
+            Client* client = data[0];
             if (data) delete[] data;
             data = tmp;
+            return client;
         }
 
                                                             // вывод данных про всех клиентов в очереди
@@ -219,7 +226,73 @@ class Stack {
         string top() {
             if (operations) return operations[size-1]; 
             else throw std::out_of_range("он пустой");
-        } 
+        }
+
+        void out_line()
+        {
+            if(operations)
+            {
+                for(size_t i = 0; i < size; ++i)
+                {
+                    std::cout << operations[i] << std::endl;
+                }
+            }
+        }
+};
+
+class BankSystem
+{
+public:
+    void add_client(int id, const std::string& requestType)
+    {
+        Client* client = new Client(id, requestType);
+        deque.back_push(client);
+    };
+
+    void request_processing()
+    {
+        std::string operation;
+        bool flag = false;
+        Client* client = deque.front_pop();
+
+        for(size_t i = 0; i < 5; ++i)
+        {
+            if(!operators[i].getStatus())
+            {
+                operation = operators[i].processRequest(client);
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag)
+            stack.push(operation);
+        else
+            throw std::invalid_argument("No free operators");
+    };
+
+    void show_queue()
+    {
+        deque.out_line();
+    };
+
+    void show_stack()
+    {
+        stack.out_line();
+    }
+
+    ~BankSystem()
+    {
+        for(size_t i = 0; i < deque.size; ++i)
+        {
+            delete deque.data[i];
+        }
+    };
+
+private:
+    Deque deque;
+    Stack stack;
+    Operator operators[5] = {Operator(1), Operator(2), Operator(3), Operator(4), Operator(5)};
 };
 //ТЗ: Нужно наши классы объединить в один: BankSystem
 //Интерфейс main как раз реализовывается через этот класс, в самом начале main можно создать один объект(например, BankSystem bank)
@@ -230,20 +303,21 @@ class Stack {
 //Если будут вопросы по реализации или по тому, как работают классы, которые мы написали до этого, можете написать в тг(@Udjin86)
 
 // тесты в main
-// int main() {
-//     Operator* operators[2];
-//     Stack stack_;
-    
-//     Client client(0, "low");
-//     Client client_1(1, "high");
-    
-//     Operator operator_(0);
-//     Operator operator_1(1);
-//     operators[0] = &operator_;
+int main() {
+    BankSystem bank;
 
-//     cout << stack_.top();
-//     stack_.push(operators[0]->processRequest(&client_1));
-//     cout << stack_.top();
+    bank.add_client(1, "low");
+    bank.add_client(2, "low");
+    bank.add_client(3, "low");
 
-//     return 0;
-// }
+    bank.request_processing();
+    bank.request_processing();
+
+    bank.request_processing();
+
+
+    bank.show_stack();
+    bank.show_queue();
+
+    return 0;
+}
